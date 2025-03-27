413b28ac148c6b897bca3a21cf38cd447ce4439e
pousone <albangruyer@gmail.com>
2025-03-27 10:25:59 +0100

    start on game init

--- AgarIO/src/main/java/iut/gon/agario/gameEngine/GameEngine.java
+++ AgarIO/src/main/java/iut/gon/agario/gameEngine/GameEngine.java
@@ -3,10 +3,17 @@ package iut.gon.agario.gameEngine;
 public class GameEngine extends Thread {
 
     private final QuadTree gameMap;
+    private final int nbPelle;
 
-    public GameEngine(double mapWidth, double mapHeight){
+    public GameEngine(double mapWidth, double mapHeight, int nbPelle){
         gameMap = QuadTree.buildEmptyTree(mapWidth, mapHeight, 6);
+        this.nbPelle = nbPelle;
+
+
+
     }
+
+
 
     @Override
     public void run(){
